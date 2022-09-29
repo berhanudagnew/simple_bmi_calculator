44d6647c05a1671bdb08526014cbaaf72e13b99d
berhanudagnew <berhanudagnew.github@gmail.com>
2022-09-29 18:53:56 +0300

    separate pages

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(BMICalculator());
-
-class BMICalculator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.light(
-          primary: Color(0xFF090C22),
-        ),
-        scaffoldBackgroundColor: Color(0xFF090C22),
-      ),
-      home: InputPage(),
-    );
-  }
-}
-
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
